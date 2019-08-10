e9dd3fb61e845312e85c57431d6f76bfb9fcbf43
Bishal Jung Rayamajhi <bishal.j.rayamajhi@gmail.com>
2019-08-11 00:39:39 +0545

    scrolling property added to header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.getElementById('burger-icon');
 const  el = document.getElementById('nav-container');
+const header = document.querySelector('.header');
 burgerBtn.addEventListener('click',showMenu);
 
 function showMenu (){
@@ -13,7 +14,28 @@ window.addEventListener('resize',windowResize);
 function windowResize(){
     if(window.innerWidth > 576){
         el.classList.remove('header__nav-container--open');
+    }
+    //removes header background if browser resizes to mobile device
+    if(window.innerWidth < 768){
+        if(header.classList.contains('header--scroll')){
+            header.classList.remove('header--scroll');
+        }
+    }
 
+
+
+}
+
+window.onscroll = function() {changeHeaderColor()}; //adds  background white on scroll to fixed header
+
+function changeHeaderColor() {
+    if(window.innerWidth > 767){
+        if (document.documentElement.scrollTop > 350) {
+            header.classList.add('header--scroll');
+        }
+        else{
+            header.classList.remove('header--scroll');
+        }
     }
 
 }
